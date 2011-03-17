04b8fdf90857bf03e2b982f6f8f83f327851d6a0
Jason Kraus <jasonk@cukerinteractive.com>
2011-03-17 14:13:01 -0700

    better memory management with saved nodes

--- transactional/handler.py
+++ transactional/handler.py
@@ -7,6 +7,57 @@ from django.utils.datastructures import SortedDict
 
 import settings
 
+class TrackableStack(object):
+    def __init__(self):
+        self.stack = list()
+        self.index = dict()
+    
+    def append(self, obj):
+        self.index[obj] = len(self.stack)
+        self.stack.append(obj)
+    
+    def remove(self, obj):
+        index = self.index[obj]
+        self.stack = self.stack[:index]
+    
+    def top(self):
+        return self.stack[-1]
+
+class TransactionalManagerContext(object):
+    context = threading.local()
+    
+    @classmethod
+    def activate_context(cls, manager):
+        cls.get_or_init().append(manager)
+    
+    @classmethod
+    def deactivate_context(cls, manager):
+        cls.get_or_init().remove(manager)
+
+    @classmethod
+    def get_or_init(cls):
+        if not hasattr(cls.context, 'managers'):
+            cls.context.managers = TrackableStack()
+        return cls.context.managers
+    
+    @classmethod
+    def get_active_context(cls):
+        managers = cls.get_or_init()
+        try:
+            return managers.top()
+        except IndexError:
+            transactional_manager = TransactionalManager()
+            transactional_manager.activate_context()
+            return transactional_manager
+
+class SavePoint(dict):
+    def __init__(self):
+        super(SavePoint, self).__init__()
+        self['_id'] = random.random()
+    
+    def __hash__(self):
+        return self['_id']
+
 def initialize_middleware(paths=None):
     middlewares = SortedDict()
     if paths is None:
@@ -54,8 +105,14 @@ class TransactionalManager(object):
             if hasattr(middleware, attr):
                 getattr(middleware, attr)(*args, **kwargs)
     
+    def activate_context(self):
+        TransactionalManagerContext.activate_context(self)
+    
+    def deactivate_context(self):
+        TransactionalManagerContext.deactivate_context(self)
+    
     def get_savepoints(self):
-        return getattr(self.local, 'savepoints', [])
+        return getattr(self.local, 'savepoints', None)
     
     def enter(self, flag=False):
         self._proxy_call('enter')
@@ -82,16 +139,18 @@ class TransactionalManager(object):
     
     def savepoint_enter(self):
         if not hasattr(self.local, 'savepoints'):
-            self.local.savepoints = list()
-        savepoint = {'_id': random.random()}
+            self.local.savepoints = TrackableStack()
+        savepoint = SavePoint()
         self.local.savepoints.append(savepoint)
         self._proxy_call('savepoint_enter', savepoint)
         return savepoint
     
     def savepoint_rollback(self, savepoint):
+        self.local.savepoints.remove(savepoint)
         self._proxy_call('savepoint_rollback', savepoint)
     
     def savepoint_commit(self, savepoint):
+        self.local.savepoints.remove(savepoint)
         self._proxy_call('savepoint_commit', savepoint)
     
     def commit_unless_managed(self):
@@ -113,35 +172,38 @@ class TransactionalManager(object):
     
     def record_action(self, path, action):
         return self.middleware[path].record_action(action)
+    
+    def __del__(self):
+        self.deactivate_context()
 
-transactional_manager = TransactionalManager()
+transactional_manager = TransactionalManagerContext.get_active_context
 
 
 def enter_transaction_management(managed=True):
-    transactional_manager.enter_transaction_management()
+    transactional_manager().enter_transaction_management()
     if managed:
-        transactional_manager.managed(managed)
+        transactional_manager().managed(managed)
 
 def leave_transaction_management():
-    transactional_manager.leave_transaction_management()
+    transactional_manager().leave_transaction_management()
 
 def is_managed():
-    return transactional_manager.is_managed()
+    return transactional_manager().is_managed()
 
 def managed(flag=True):
-    return transactional_manager.managed(flag)
+    return transactional_manager().managed(flag)
 
 def commit_unless_managed():
-    transactional_manager.commit_unless_managed()
+    transactional_manager().commit_unless_managed()
 
 def rollback_unless_managed():
-    transactional_manager.rollback_unless_managed()
+    transactional_manager().rollback_unless_managed()
 
 def commit():
-    transactional_manager.commit()
+    transactional_manager().commit()
 
 def rollback():
-    transactional_manager.rollback()
+    transactional_manager().rollback()
 
 def savepoint():
     """
@@ -149,20 +211,20 @@ def savepoint():
     current transaction. Returns an identifier for the savepoint that will be
     used for the subsequent rollback or commit.
     """
-    return transactional_manager.savepoint()
+    return transactional_manager().savepoint()
 
 def savepoint_rollback(sid):
     """
     Rolls back the most recent savepoint (if one exists). Does nothing if
     savepoints are not supported.
     """
-    transactional_manager.savepoint_rollback(sid)
+    transactional_manager().savepoint_rollback(sid)
 
 def savepoint_commit(sid):
     """
     Commits the most recent savepoint (if one exists). Does nothing if
     savepoints are not supported.
     """
-    transactional_manager.savepoint_commit(sid)
+    transactional_manager().savepoint_commit(sid)
 
 
